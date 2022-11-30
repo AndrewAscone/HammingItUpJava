baca879ffa41d8620452dddbe3a484921bebc58d
AndrewAscone <andrew.ascone@gmail.com>
2022-11-29 21:32:18 -0500

    Hamming methods completed, tests pass

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
-package zipcode;
+package src.main.zipcode;
 
 public class Hamming {
+    String s;
+
+     String s1;
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if(s.length() != s1.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int hamCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != s1.charAt(i)){
+                hamCount++;
+            }
+        }
+        return hamCount;
     }
 }
